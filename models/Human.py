0ca104ed310d821c8001726601176e676f0f7c82
xiurobert <xiurobert@users.noreply.github.com>
2017-03-30 15:31:06 +0800

    Because @greysome complained it's bad practise

--- models/Human.py
+++ models/Human.py
@@ -1,1 +1,12 @@
-class Human():
+class Humam():
+    def __init__(self, height = 0, weight = 0, IQ = 100, firstName = "Jotham", lastName = "Lim", deathAge = 80, medicalConditions = [],
+                gender = "Gay"):
+        self.height = height
+        self.weight = weight
+        self.IQ = IQ
+        self.firstName = firstName
+        self.lastName = lastName
+        self.deathAge = deathAge
+        self.medicalCondition = medicalConditions
+        self.gender = gender
+        
